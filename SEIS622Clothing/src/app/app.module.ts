fc59df01673c7c4df9d4062bf38ef24d2208a068
yang7656 <55370158+yang7656@users.noreply.github.com>
2023-11-23 00:48:55 -0600

    load

--- SEIS622Clothing/src/app/app.module.ts
+++ SEIS622Clothing/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewArrivalComponent } from './new-arrival/new-arrival.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewArrivalComponent
   ],
   imports: [
     BrowserModule
