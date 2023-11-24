86092e41fe464394d80bfd1e000759d5ba3b1d76
yang7656 <55370158+yang7656@users.noreply.github.com>
2023-11-24 10:40:04 -0600

    api

--- SEIS622Clothing/serve/api.js
+++ SEIS622Clothing/serve/api.js
@@ -1,23 +1,25 @@
-const express = require('express');
-const fs = require('fs');
 const path = require('path');
-
+const express = require('express');
 const app = express();
-const imagesDirectory = path.join(__dirname, 'src/assets/images');
+const port = 3000;
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const imagesDirectory = path.join(__dirname, '../src/assets/images');
+
+let imageFiles;
+
+fs.readdir(imagesDirectory, (err, files) => {
+  if (err) 
+  {
+    res.status(500).send('Error reading directory');
+    return;
+  }
+  imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif)$/.test(file));
+});
 
 app.get('/images', (req, res) => 
 {
-  fs.readdir(imagesDirectory, (err, files) => 
-  {
-    if (err) 
-    {
-      res.status(500).send('Error reading directory');
-      return;
-    }
-    const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif)$/.test(file));
-    res.json(imageFiles);
-  });
+  res.json(imageFiles);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(port, () => console.log("Running on port " + port));
