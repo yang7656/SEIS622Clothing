69e0ffb7cc3dad9cf853c65287f71f7c4a924537
imafink <52085190+imafink@users.noreply.github.com>
2023-12-10 13:42:28 -0600

    Fixed dropdown menu and changed routing

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -4,14 +4,11 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ShoppingComponent } from './shopping/shopping.component';
 
-// Routes for child Module (publicModule). Since public module is lazy loaded in the app-routing.module the full path is `/public/*`
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'protected/cart', loadChildren: () => import('../protected/protected.module').then(m => m.ProtectedModule) },
-  { path: 'protected/myaccount', loadChildren: () => import('../protected/protected.module').then(m => m.ProtectedModule) },
-  { path:'', component: ShoppingComponent },
-  { path: '**', redirectTo: 'login', pathMatch: 'full' }
+  { path: '', component: ShoppingComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
