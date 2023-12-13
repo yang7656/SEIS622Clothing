d2de2c6e59a621d07b764bd8ffd360ae70272bb5
yang7656 <55370158+yang7656@users.noreply.github.com>
2023-12-13 03:14:29 -0600

    final adjustment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, Event, RoutesRecognized, RouteConfigLoadStart, NavigationEnd, ActivatedRoute } from '@angular/router';
-import { filter, map } from 'rxjs/operators';
 import { AuthService } from './shared/services/auth.service';
 
 @Component({
@@ -15,29 +14,7 @@ export class AppComponent implements OnInit {
   constructor(private router: Router, private auth: AuthService) {}
   
   ngOnInit() {
-
-    this.checkIfLoggedIn();
-
-    this.router.events.subscribe((event: Event) => {
-      if (event instanceof RoutesRecognized) {
-        //console.log('RoutesRecognized:', event);
-      }
-
-      if (event instanceof RouteConfigLoadStart) {
-        //console.log('RouteConfigLoadStart:', event);
-      }
-    });
-
-  }
-
-  checkIfLoggedIn() {
-    this.auth.isLoggedIn().subscribe({
-      next: (isLoggedIn) => {
-        if (!isLoggedIn) {
-          this.router.navigate(['public']);
-        }
-      }
-    });
+    this.auth.logout();
   }
 
 }
